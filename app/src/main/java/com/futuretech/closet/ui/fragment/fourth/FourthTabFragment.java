8c372cb3bd0e6c645a227fcc5c359439ceea1a3c
BuildTools <unconfigured@null.spigotmc.org>
2019-03-29 16:45:42 +0800

    添加第四页面

--- app/src/main/java/com/futuretech/closet/ui/fragment/fourth/FourthTabFragment.java
+++ app/src/main/java/com/futuretech/closet/ui/fragment/fourth/FourthTabFragment.java
@@ -9,10 +9,13 @@ import android.view.ViewGroup;
 
 import com.futuretech.closet.R;
 import com.futuretech.closet.base.BaseMainFragment;
+import com.futuretech.closet.ui.fragment.fourth.child.AvatarFragment;
+import com.futuretech.closet.ui.fragment.fourth.child.MeFragment;
 
 public class FourthTabFragment extends BaseMainFragment {
 
-    Toolbar toolbar;
+    private Toolbar mToolbar;
+    private View mView;
 
     public static FourthTabFragment newInstance() {
 
@@ -26,20 +29,32 @@ public class FourthTabFragment extends BaseMainFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_tab_fourth, container, false);
-        initView(view);
-        return view;
+        mView = inflater.inflate(R.layout.fragment_tab_fourth, container, false);
+        initView(mView);
+        return mView;
     }
 
     private void initView(View view) {
-        toolbar = view.findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.me);
     }
+
+
 
     @Override
     public void onLazyInitView(@Nullable Bundle savedInstanceState) {
         super.onLazyInitView(savedInstanceState);
 
+        if (findChildFragment(AvatarFragment.class) == null) {
+            loadFragment();
+        }
 
+        mToolbar = (Toolbar) mView.findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.me);
     }
+
+    private void loadFragment() {
+        loadRootFragment(R.id.fl_fourth_container_upper, AvatarFragment.newInstance());
+        loadRootFragment(R.id.fl_fourth_container_lower, MeFragment.newInstance());
+    }
+
+
 }
---
+++ app/src/main/java/com/futuretech/closet/ui/fragment/fourth/child/AvatarFragment.java
@@ -0,0 +1,32 @@
+package com.futuretech.closet.ui.fragment.fourth.child;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.yokeyword.fragmentation.SupportFragment;
+import com.futuretech.closet.R;
+
+/**
+ * Created by YoKeyword on 16/6/6.
+ */
+public class AvatarFragment extends SupportFragment {
+
+    public static AvatarFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        AvatarFragment fragment = new AvatarFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tab_fourth_avatar, container, false);
+        return view;
+    }
+}
