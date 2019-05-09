ea49aacbb8206b436f98191c16017d81d257ad1f
Sakari <sk308269317@outlook.com>
2019-05-09 18:30:51 +0800

    添加套装页面

---
+++ app/src/main/java/com/futuretech/closet/ui/fragment/central/CentralTabFragment.java
@@ -0,0 +1,45 @@
+package com.futuretech.closet.ui.fragment.central;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.futuretech.closet.R;
+import com.futuretech.closet.base.BaseMainFragment;
+
+public class CentralTabFragment extends BaseMainFragment {
+
+    Toolbar toolbar;
+
+    public static CentralTabFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        CentralTabFragment fragment = new CentralTabFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tab_central, container, false);
+        initView(view);
+        return view;
+    }
+
+    private void initView(View view) {
+        toolbar = view.findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.suit);
+    }
+
+    @Override
+    public void onLazyInitView(@Nullable Bundle savedInstanceState) {
+        super.onLazyInitView(savedInstanceState);
+
+
+    }
+}
